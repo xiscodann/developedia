32cbe3db982aac3c1c347a415cdea9d80725e4b2
Daniel Ruiz <daniel.ruiz@rapidfire.com>
2023-03-01 14:47:05 -0500

    Finished the creation of the Backend for developedia

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,9 +2,9 @@ import jwt from 'jsonwebtoken';
 
 export const verifyToken = async (req, res, next) => {
   try {
-    let token = req.headers('Authorization');
+    let token = req.headers['x-auth-token'];
 
-    if (!token) return res.status(403).send('Acces Denied');
+    if (!token) return res.status(403).send('Access Denied');
 
     if (token.startsWith('Bearer ')) {
       token = token.slice(7, token.length).trimLeft();
